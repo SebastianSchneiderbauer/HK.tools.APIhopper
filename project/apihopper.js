ac5513d53220d7dc0ca18209b0bcf3e9f5ca66e8
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2025-06-26 19:19:02 +0200

    add load requirement and getAPIname

--- project/apihopper.js
+++ project/apihopper.js
@@ -1,14 +1,36 @@
 //APIhopper by Sebastian Schneiderbauer aka HerrKleiderbauer
 var apikeys
+var APIkeysloaded = false
 
 export function loadAPIkeys(){
     apikeys = []
     apikeys.push(["test1","1","2"])
     apikeys.push(["test2","1","2"])
+    APIkeysloaded = true
+}
+
+export function getAPIname(index){
+    if (APIkeysloaded === false){
+        return "APIkeys are not loaded, call \"loadAPIkeys\" first"
+    }
+
+    if (typeof(index) != "number"){
+        return "\"index\" parameter is not of the right type (needs a number). Provided: " + typeof(index)
+    }
+
+    if (index < 0){
+        return "\"index\" parameter needs to be greater than 0. Provided: " + index
+    }
+
+    console.log(apikeys[index][0])   
 }
 
 export function callAPI(index,call){
     // handle error cases
+    if (APIkeysloaded === false){
+        return "APIkeys are not loaded, call \"loadAPIkeys\" first"
+    }
+
     if (typeof(index) != "number"){
         return "\"index\" parameter is not of the right type (needs a number). Provided: " + typeof(index)
     }
@@ -21,6 +43,6 @@ export function callAPI(index,call){
         return "\"call\" parameter is not of the right type (needs a string). Provided: " + typeof(index)
     }
 
-    var keys = apikeys[index].splice(1)
+    var keys = apikeys[index].splice(1) //remove api name entry
     console.log(keys)   
 }
--- project/index.js
+++ project/index.js
@@ -1,6 +1,6 @@
 //this will contain code to test the program
-import { callAPI,loadAPIkeys } from "./apihopper.js"
+import { callAPI,loadAPIkeys,getAPIname } from "./apihopper.js"
 
 loadAPIkeys()
 callAPI(0,"r")
-callAPI(1,"r")
+getAPIname(1)
