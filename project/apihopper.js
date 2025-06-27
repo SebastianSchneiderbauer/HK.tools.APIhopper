7337fc725a9aec769e1fc220a1b22022d2602e1d
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2025-06-27 11:09:06 +0200

    fix getAPIname and clean up code

--- project/apihopper.js
+++ project/apihopper.js
@@ -3,10 +3,13 @@
 var apikeys = []
 
 export function addAPIkeyCollection(keys){
-    apikeys.push(keys)
+    if (keys.length >= 4){
+        apikeys.push(keys)
+    }
 }
 
 export function getAPIname(index){
+    // handle error cases
     if (apikeys.length === 0){
         return "There are no APIkeyCollections loaded right now. Use \"addAPIkeyCollection(\" to load them"
     }
@@ -23,7 +26,7 @@ export function getAPIname(index){
         return "There is no APIkeyCollection with index: " + index
     }
 
-    console.log(apikeys[index][0])   
+    return apikeys[index][0]
 }
 
 export async function callAPI(index,call){
@@ -47,26 +50,27 @@ export async function callAPI(index,call){
     if (typeof(call) != "string"){
         return "\"call\" parameter is not of the right type (needs a string). Provided: " + typeof(index)
     }    
+
     //initialize the checkvariables, we assume they are correct
     var checkField = apikeys[index][1]
     var checkValue = apikeys[index][2]
 
+    //cut apikeys to get only the needed APIkeys
     var keys = apikeys[index].slice(3) //get elements from index 3 onwards without modifying original
     
     var loopIndex = 0
     var result
-    var response = null
+    var toCheckValue = null
     var callparts = call.split("APIKEY")
 
-    while (response != checkValue && loopIndex < keys.length){
+    while (toCheckValue != checkValue && loopIndex < keys.length){
         result = await fetch(callparts[0] + keys[loopIndex] + callparts[1])
-        response = (result[checkField]).toString()
-        //console.log(response === checkValue)
+        toCheckValue = (result[checkField]).toString()
 
         loopIndex += 1 
     }
 
-    if (response === checkValue){
+    if (toCheckValue === checkValue){
         return result
     }
     else{
--- project/index.js
+++ project/index.js
@@ -5,8 +5,10 @@ dotenv.config();
 
 addAPIkeyCollection(["marketstack","status","200",process.env.MARKETSTACK_API_KEY_FAKE, process.env.MARKETSTACK_API_KEY_REAL])
 
-getAPIname(0)
+//get the APIkeyCollection´s name
+console.log(getAPIname(0))
 
+//call the 0´th API
 var result = await callAPI(0,"http://api.marketstack.com/v1/eod?access_key=APIKEY&symbols=AAPL")
 
 console.log(result)
